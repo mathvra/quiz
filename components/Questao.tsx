02488950a9c0bcaf2dc7bd56c9746b4012b57260
mathvra <matheusvram@gmail.com>
2021-08-26 13:25:16 -0300

    alteracao do CSS e comunicacao indireta

--- components/Questao.tsx
+++ components/Questao.tsx
@@ -3,8 +3,16 @@ import QuestaoModel from "../model/questao";
 import Enunciado from './Enunciado';
 import Resposta from './Resposta';
 
+const letras = [
+    {valor: 'A', cor: '#F2C866'},
+    {valor: 'B', cor: '#F266BA'},
+    {valor: 'C', cor: '#85D4F2'},
+    {valor: 'D', cor: '#BCE596'},
+]
+
 interface QuestaoProps {
     valor: QuestaoModel
+    respostaFornecida: (indice: number) => void
 }
 
 export default function Questao(props: QuestaoProps) {
@@ -13,16 +21,18 @@ export default function Questao(props: QuestaoProps) {
     function renderizarRespostas() {
         return questao.respostas.map((resposta, i) => {
             return (
-                <Resposta
+                <Resposta 
+                    key={i}
                     valor={resposta}
                     indice={i}
-                    letra="A"
-                    corLetra="#F2C866"
+                    letra={letras[i].valor}
+                    corFundoLetra={letras[i].cor}
+                    respostaFornecida={props.respostaFornecida}
                 />
             )
         })
     }
- 
+
     return(
         <div className={styles.questao}>
             <Enunciado texto={questao.enunciado}/>
--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -5,16 +5,19 @@ interface RespostaProps {
     valor: RespostaModel
     indice: number
     letra: string
-    corLetra: string
+    corFundoLetra: string
+    respostaFornecida: (indice: number) => void
 }
 
 export default function Resposta(props: RespostaProps){
     const resposta = props.valor
     return (
-        <div className={styles.resposta}>
+        <div className={styles.resposta}
+            onClick={() => props.respostaFornecida(props.indice)}>
             <div className={styles.conteudoResposta}>
                 <div className={styles.frente}>
-                    <div className={styles.letra}>
+                    <div className={styles.letra}
+                        style={{backgroundColor: props.corFundoLetra}}>
                         {props.letra}
                     </div>
                     <div className={styles.valor}>
